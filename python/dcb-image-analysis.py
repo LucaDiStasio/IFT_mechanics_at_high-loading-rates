a800ead2a03f698a75bde98891e0979f54ec6a50
LucaDiStasio <luca.distasio@gmail.com>
2018-02-28 18:14:58 +0100

    commit on 2018/02/28 at 18:14:58

--- python/dcb-image-analysis.py
+++ python/dcb-image-analysis.py
@@ -35,19 +35,62 @@ import numpy as np
 import cv2
 from matplotlib import pyplot as plt
 
+plt.close('all')
+
 img = cv2.imread('LAM365-1-s-0329_0.tif',0)
 height,width = img.shape
 
+img2 = img.copy()
+
+template = cv2.imread('lower-hinge.tif',0)
+w, h = template.shape[::-1]
+
+template2 = cv2.imread('upper-hinge.tif',0)
+w2, h2 = template2.shape[::-1]
+
+template3 = cv2.imread('crack-tip.tif',0)
+w3, h3 = template3.shape[::-1]
+
+res = cv2.matchTemplate(img,template,cv2.TM_CCOEFF_NORMED)
+min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(res)
+
+top_left = max_loc
+bottom_right = (top_left[0] + w, top_left[1] + h)
+
+res2 = cv2.matchTemplate(img,template2,cv2.TM_CCOEFF_NORMED)
+min_val2, max_val2, min_loc2, max_loc2 = cv2.minMaxLoc(res2)
+
+top_left2 = max_loc2
+bottom_right2 = (top_left2[0] + w2, top_left2[1] + h2)
+
+res3 = cv2.matchTemplate(img,template3,cv2.TM_CCOEFF_NORMED)
+min_val3, max_val3, min_loc3, max_loc3 = cv2.minMaxLoc(res3)
+
+top_left3 = max_loc3
+bottom_right3 = (top_left3[0] + w3, top_left3[1] + h3)
+
+cv2.rectangle(img,top_left, bottom_right, 255, 4)
+cv2.rectangle(img,top_left2, bottom_right2, 255, 4)
+cv2.rectangle(img,top_left3, bottom_right3, 255, 4)
+
+plt.subplot(121),plt.imshow(img2,cmap = 'gray')
+plt.title('Original'), plt.xticks([]), plt.yticks([])
+plt.subplot(122),plt.imshow(img,cmap = 'gray')
+plt.title('Detected Point'), plt.xticks([]), plt.yticks([])
+
+
+plt.show()
+
 #cv2.imwrite('trial.png',img)
-ret,th = cv2.threshold(img,127,255,cv2.THRESH_BINARY)
+#ret,th = cv2.threshold(img,127,255,cv2.THRESH_BINARY)
 #th = cv2.adaptiveThreshold(img,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,cv2.THRESH_BINARY,11,2)
 
-surf = cv2.SURF(30000)
-kp, des = surf.detectAndCompute(th,None)
-print len(kp)
-img2 = cv2.drawKeypoints(th,kp,None,(255,0,0),4)
+#surf = cv2.SURF(30000)
+#kp, des = surf.detectAndCompute(th,None)
+#print len(kp)
+#img2 = cv2.drawKeypoints(th,kp,None,(255,0,0),4)
 
-edges = cv2.Canny(img,100,200)
+#edges = cv2.Canny(img,100,200)
 
 #cv2.line(img,(0,0),(width,height),(0,0,255),5)
 
@@ -55,11 +98,11 @@ edges = cv2.Canny(img,100,200)
 
 #cv2.namedWindow(imageTitle)
 
-fig, (ax1, ax2, ax3) = plt.subplots(1,3)
-ax1.imshow(img, cmap = 'gray', interpolation = 'bicubic')
+#fig, (ax1, ax2, ax3) = plt.subplots(1,3)
+#ax1.imshow(img, cmap = 'gray', interpolation = 'bicubic')
 #ax2.imshow(th, cmap = 'gray')
-ax2.imshow(img2)
-ax3.imshow(edges, cmap='gray')
+#ax2.imshow(img2)
+#ax3.imshow(edges, cmap='gray')
 #plt.xticks([]), plt.yticks([])  # to hide tick values on X and Y axis
-plt.tight_layout()
-plt.show()
+#plt.tight_layout()
+#plt.show()
